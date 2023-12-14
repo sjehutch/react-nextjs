9acdb3d3f6089a15e9c450d140935ee130aefa86
Scott Hutchinson <sjehutch@gmail.com>
2023-12-13 19:45:13 -0600

    update
    update

---
+++ src/app/account/page.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const UsersPage = () => {
+  return (
+    <div className="bg-white dark:bg-black">
+      <Typography variant="h3" component="h2" gutterBottom>
+        Users Page
+      </Typography>
+    </div>
+  );
+};
+
+export default UsersPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Next.js</h1>
       <Link href="/users">Users</Link>
-      </main>
- 
-  )
+      <br />
+      <Link href="/account">Accounts</Link>
+    </main>
+  );
 }
